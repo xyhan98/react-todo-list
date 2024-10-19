e5d355d98d2669c8bcc221eeeb5ff5e361d09f72
xinyi.han <xinyi.han98@outlook.com>
2024-10-19 17:27:39 +0800

    config project

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom'
 import App from './App.jsx'
 import './index.css'
 
-createRoot(document.getElementById('root')).render(
+ReactDOM.render(
   <StrictMode>
     <App />
   </StrictMode>,
+  document.getElementById('root')
 )
